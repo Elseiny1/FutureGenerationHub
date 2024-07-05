ea33d43cc9e147686f538a71b0913d883f7ea777
ELSEINY <ahmedelseiny211@gmail.com>
2024-07-05 14:22:58 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FutureGenerationHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
